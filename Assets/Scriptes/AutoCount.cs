665c52527baa9202c98651b2489cb4859446cf6a
近藤　倫太 <tora.goraias@gmail.com>
2022-06-07 20:35:00 +0900

    コミット

--- Assets/Scriptes/AutoCount.cs
+++ Assets/Scriptes/AutoCount.cs
@@ -7,40 +7,50 @@ using System.IO;
 public class AutoCount : MonoBehaviour
 {
     [SerializeField]
-    [Header("ƒAƒCƒeƒ€‚Ì‰¿Ši")] int _price  = 50;
+    [Header("ƒAƒCƒeƒ€‚Ì‰¿Ši")]
+    int _price  = 50;
 
     [SerializeField]
-    [Header("1‰ñ¶ŽY‚·‚éŽžŠÔ(•b)")] int _productSpeed = 10;
+    [Header("1‰ñ¶ŽY‚·‚éŽžŠÔ(•b)")] 
+    int _productSpeed = 10;
 
     [SerializeField]
-    [Header("1‰ñ‚É¶ŽY‚·‚é”")] int _product = 1;
+    [Header("1‰ñ‚É¶ŽY‚·‚é”")] 
+    int _product = 1;
 
-    [Header("w“ü‰ñ”")] int _clickCounter;
+    [Header("w“ü‰ñ”")] 
+    int _clickCounter;
 
     private GrapeCounter _grapeCounter = new GrapeCounter();
 
-    public void OnClick()
+    public void OnBuyAutoCounter()
     {
-        if(_grapeCounter.Grape > _price && _clickCounter < 1)
+        if (_grapeCounter.Grape > _price)
         {
-            AutoProduct();
-            _clickCounter++;
+            if (_clickCounter < 1)
+            {
+                AutoProduct();
+                _clickCounter++;
+            }
+            else
+            {
+                _product *= 2;
+                _clickCounter++;
+            }
+            if (_clickCounter % 5 == 0 && _clickCounter != 0)
+            {
+                _productSpeed -= 1;
+            }
+            _price *= 2;
         }
-        if(_grapeCounter.Grape > _price && _clickCounter > 0)
-        {
-            _product *= 2;
-            _clickCounter++;
-        }
-        if(_clickCounter % 5 == 0)
-        _price *= 2;
     }
     private IEnumerator AutoProduct()
     {
         while(true)
         {
-            _grapeCounter.Grape = + _product;
+            _grapeCounter.Grape += _product;
             yield return new WaitForSeconds(_productSpeed);
-            _grapeCounter.Grape = + _product;
+            _grapeCounter.Grape += _product;
         }
     }
 }
